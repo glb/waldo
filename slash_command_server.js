b8d21385c97c63d85d2c6fe42ac78a68a2097c37
Morgan <Morgan35@users.noreply.github.com>
2016-12-07 14:04:49 -0500

    Appease auto-formatter and add more help/error messages

--- slash_command_server.js
+++ slash_command_server.js
@@ -27,30 +27,20 @@ app.route('/whereis')
 
     // Handle any help requests
     if (req.body.text === 'help') {
-      message = "Need a hand? Try this!"
-    }
-    // Check if user exists in Slack
-    else if (true) {
-      // Easter egg: @waldo
-      if (req.body.text === '@waldo') {
-        message = "20000 leagues under the sea!"
-      }
-      // Easter egg: @glb
-      else if (req.body.text === '@glb') {
+      message = 'Need a hand? Try this!'
+    } else if (true) {  // Check if user exists in Slack
+      var user = req.body.text
+      if (user === '@waldo') {       // Easter egg: @waldo
+        message = '20000 leagues under the sea!'
+      } else if (user === '@glb') {  // Easter egg: @glb
         // later call Waldo bot to message with /shrug
-      }
-      // Check if user exists in db location table
-      else if (true) {
+      } else if (false) {                      // Check if user exists in db location table
 
+      } else {                                // Return suggestion to talk to @waldo to add location
+        message = 'I don\'t know where ' + user + ' is.  If you find out, please message @waldo and tell me!'
       }
-      // Return suggestion to talk to @waldo to add location
-      else {
-
-      }
-    }
-    // Return error bc user does not exist in Slack
-    else {
-
+    } else {                                  // Return error bc user does not exist in Slack
+      message = 'Hmm... ' + req.body.text + 'doesn\'t seem to be a current Slack user!'
     }
 
     res.json({
