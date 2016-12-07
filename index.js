a9246c230239f1076d02e6b101c71b5d3ce10d81
Morgan <Morgan35@users.noreply.github.com>
2016-12-07 16:46:29 -0500

    Try to connect to the db and setup main table

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 var childProcess = require('child_process')
 
+var pg = require('pg')
+var DB_URL = process.env.DATABASE_URL
+
+pg.defaults.ssl = true
+pg.connect(DB_URL, function (err, client) {
+  if (err) {
+    console.log('Failed to connect to postgres: ' + err)
+  }
+
+  client
+    .query(`CREATE TABLE IF NOT EXISTS user_locations (
+      username    varchar(22) PRIMARY KEY,
+      office      varchar,
+      floor       varchar,
+      seat        integer
+    ); `, function (err, result) {
+      if (err) {
+        console.log('Error querying database: ' + err)
+      } else {
+        console.log('User location table exists or was created')
+      }
+    })
+})
+
 // Setup slack bot
 childProcess.spawn('node', ['slackbot.js'], {stdio: 'inherit'})
 
