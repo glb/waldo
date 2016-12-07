a9629951acad2a4dae05ab88ba743419b2c1236c
Graham McGregor <graham@gmcgregor.ca>
2016-12-07 10:46:52 -0500

    Basic server

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 #!/usr/bin/env node
+var express = require('express')
+var bodyParser = require('body-parser')
 
-console.log('hi')
+var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN
+if (!VERIFY_TOKEN) {
+  console.error('SLACK_VERIFY_TOKEN is required')
+  process.exit(1)
+}
+var PORT = process.env.PORT || 45678
+if (!PORT) {
+  console.error('PORT is required')
+  process.exit(1)
+}
+
+var app = express()
+
+app.route('/whereis')
+  .get(function (req, res) {
+    res.sendStatus(200)
+  })
+  .post(bodyParser.urlencoded({ extended: true }), function (req, res) {
+    if (req.body.token !== VERIFY_TOKEN) {
+      return res.sendStatus(401)
+    }
+
+    var message = 'Hold on, I\'m consulting a map...'
+
+    // Handle any help requests
+    if (req.body.text === 'help') {
+      message = "HELP, YOU'RE LOST"
+    }
+
+    res.json({
+      response_type: 'ephemeral',
+      text: message
+    })
+  })
+
+app.listen(PORT, function (err) {
+  if (err) {
+    return console.error('Error starting server: ', err)
+  }
+
+  console.log('Server successfully started on port %s', PORT)
+})
